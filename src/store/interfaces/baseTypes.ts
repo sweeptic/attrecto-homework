3b28cebf14c53a38dc7b8ed2ff9510b85a1dfa5d
Attila Horvath <attila.horvath@memoq.com>
2022-05-20 12:16:32 +0200

    clean up store types #2

--- src/store/interfaces/baseTypes.ts
+++ src/store/interfaces/baseTypes.ts
@@ -1,9 +1,3 @@
-import { IErrorObject } from "store/middleware/core/api";
-import { MovieResponseData } from "store/reducers/moviesReducer";
-
-export type TErrorObject = string | IErrorObject;
-export type TResponseObject = MovieResponseData;
-
 export interface IAction {
   type: string;
 }
